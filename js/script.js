96cd86a08183e8879c60fa2c9492a11c49a26c85
Alina Gladkova <a-g1997@yandex.ru>
2020-06-30 14:12:48 +0300

    Сделала табы

--- js/script.js
+++ js/script.js
@@ -11,3 +11,17 @@ $(window).scroll(function () {
 $(".top-btn").click(function () {
   $("html, body").animate({ scrollTop: 0 }, 1000);
 });
+
+  $(function() {
+    $("ul.tabs__caption").on("click", "li:not(.active)", function() {
+      $(this)
+        .addClass("active")
+        .siblings()
+        .removeClass("active")
+        .closest("div.tabs")
+        .find("div.tabs__content")
+        .removeClass("active")
+        .eq($(this).index())
+        .addClass("active");
+    });
+  });
